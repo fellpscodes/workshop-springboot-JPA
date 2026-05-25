568e58f7940c576cc23c452f1a7ea52b9a088467
Felipe <felipesc2007@gmail.com>
2026-05-25 19:42:00 -0300

    Project first commit

---
+++ src/main/java/com/projectjava/felps/FelpsApplication.java
@@ -0,0 +1,13 @@
+package com.projectjava.felps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FelpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FelpsApplication.class, args);
+	}
+
+}
