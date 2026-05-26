0bed13ffd48743c03350bcea289307b9a32a1a50
Felipe <felipesc2007@gmail.com>
2026-05-26 15:02:17 -0300

    Service layer and component registration

---
+++ src/main/java/com/projectjava/felps/services/UserService.java
@@ -0,0 +1,25 @@
+package com.projectjava.felps.services;
+
+import com.projectjava.felps.entities.User;
+import com.projectjava.felps.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
