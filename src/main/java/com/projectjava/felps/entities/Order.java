96a87293f411d16f5b4a82776d445640af7bb9dd
Felipe <felipesc2007@gmail.com>
2026-05-26 21:00:01 -0300

    OrderStatus enum

--- src/main/java/com/projectjava/felps/entities/Order.java
+++ src/main/java/com/projectjava/felps/entities/Order.java
@@ -1,6 +1,7 @@
 package com.projectjava.felps.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.projectjava.felps.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -22,6 +23,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -29,9 +32,10 @@ public class Order implements Serializable {
     public Order(){
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -58,6 +62,16 @@ public class Order implements Serializable {
     public void setClient(User client) {
         this.client = client;
     }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null){
+        this.orderStatus = orderStatus.getCode();
+    }
+        }
 
     @Override
     public boolean equals(Object o) {
